cf6d615e4b16578d2c01734efeba86c6e5454e7c
kamilsendecki <sendej@gmail.com>
2020-04-30 17:21:47 +0200

    Updates

---
+++ precache-manifest.47f6ea19f335df06dd8b8b428f9ee523.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd36a276b28dbdd27d3549f135bb0275",
+    "url": "/ekeyboard/index.html"
+  },
+  {
+    "revision": "49f8bb4703ff4e177e74",
+    "url": "/ekeyboard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9564401ec33b9ed2487d",
+    "url": "/ekeyboard/static/js/2.3b756ade.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ekeyboard/static/js/2.3b756ade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f8bb4703ff4e177e74",
+    "url": "/ekeyboard/static/js/main.24af8032.chunk.js"
+  },
+  {
+    "revision": "9a90d47f0da1f85d8540",
+    "url": "/ekeyboard/static/js/runtime-main.4cb8a62e.js"
+  },
+  {
+    "revision": "e56a32f9a3ff7d84237f212c00c6d46d",
+    "url": "/ekeyboard/static/media/A.e56a32f9.wav"
+  },
+  {
+    "revision": "c797eb2d67469d83bc9189e947883bf6",
+    "url": "/ekeyboard/static/media/C.c797eb2d.wav"
+  },
+  {
+    "revision": "6399b14085ee308452af60b751cab377",
+    "url": "/ekeyboard/static/media/D.6399b140.wav"
+  },
+  {
+    "revision": "e56fe5ff804218cc6ee749dbf126498b",
+    "url": "/ekeyboard/static/media/E.e56fe5ff.wav"
+  },
+  {
+    "revision": "3b69d12725f464a3439e79a457e5b256",
+    "url": "/ekeyboard/static/media/F.3b69d127.wav"
+  },
+  {
+    "revision": "0cefe7d43ad941cf5ada3f6853041f21",
+    "url": "/ekeyboard/static/media/G.0cefe7d4.wav"
+  },
+  {
+    "revision": "ed6e3a4a93c27a31edac03071b55fae4",
+    "url": "/ekeyboard/static/media/H.ed6e3a4a.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ekeyboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
